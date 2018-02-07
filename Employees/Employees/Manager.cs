5a270f19c94ebbe32b9d73f11162a265d4eafdf4
Jelllier <Jelllier@mail.ru>
2018-02-07 14:08:42 +0700

    Add constructors

--- Employees/Employees/Manager.cs
+++ Employees/Employees/Manager.cs
@@ -8,6 +8,13 @@ namespace Employees
 {
     class Manager : Employee
     {
+        public Manager() { }
+        public Manager(string fullName, int age, int empID,
+                        float currPay, string ssn, int numbOfOpts)
+            :base(fullName, age, empID, currPay, ssn)
+        {
+            StockOptions = numbOfOpts;
+        }
         public int StockOptions { get; set; }
     }
 }
--- Employees/Employees/Salesperson.cs
+++ Employees/Employees/Salesperson.cs
@@ -8,6 +8,13 @@ namespace Employees
 {
     class Salesperson : Employee
     {
+        public Salesperson() { }
+        public Salesperson(string fullName, int age, int empID,
+                        float currPay, string ssn, int numbOfSales)
+            :base(fullName, age, empID, currPay, ssn)
+        {
+           SalesNumber = numbOfSales;
+        }
         public int SalesNumber { get; set; }
     }
 }

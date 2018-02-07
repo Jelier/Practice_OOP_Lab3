a0f0397b008cc25e36067369ad065b3af2dba5dc
Jelllier <Jelllier@mail.ru>
2018-02-07 13:27:37 +0700

    Create class MiniVan

--- BasicInheritance/BasicInheritance/Program.cs
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -11,8 +11,11 @@ namespace BasicInheritance
         static void Main(string[] args)
         {
             Car myCar = new Car(80);
+            MiniVan myVan = new MiniVan();
+            myVan.Speed = 10;
             myCar.Speed = 50;
             Console.WriteLine("MyCar is going {0} MPH", myCar.Speed);
+            Console.WriteLine("MyVan is going {0} MPH", myVan.Speed);
             Console.ReadLine();
         }
     }
